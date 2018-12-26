60251829d0798550819a608a47927296194c00e7
jam1garner <jam1.mcleod@hotmail.com>
2018-12-26 15:47:42 -0500

    Add script for removing duplicates

---
+++ remove_duplicates.py
@@ -0,0 +1,14 @@
+with open("ParamLabels.csv") as f:
+    csv = [line[:-1].split(',') for line in f.readlines() if not line.isspace()]
+
+hashes = []
+csvout = []
+for i in csv:
+    if not i[0] in hashes:
+        hashes.append(i[0])
+        csvout.append(i)
+
+csvout.sort(key=lambda i: i[1])
+
+with open("ParamLabels.csv", 'w') as f:
+    f.write('\n'.join([','.join(line) for line in csvout]))
