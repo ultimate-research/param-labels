2c6868fbc4525fdc8eb2776d655938bcbfcd2ac9
jam1garner <jam1.mcleod@hotmail.com>
2018-12-27 15:59:16 -0500

    Update tests for new hash format

--- test_hashes.py
+++ test_hashes.py
@@ -10,4 +10,5 @@ def test_hashes():
     
     assert_is_sorted(csv, key=lambda i: i[1])
     for line in csv:
-        assert crc32(line[1].encode('utf-8')) == int(line[0], 16)
+        # Only the lower 32 bits are the hash (blame arthur), ensure the crc is legit
+        assert crc32(line[1].encode('utf-8')) == (int(line[0], 16) & 0xFFFFFFFF)
