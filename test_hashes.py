fd0c3a3b4f5f19f0b842e448accf83ea3197fdfd
jam1garner <jam1.mcleod@hotmail.com>
2018-12-30 19:39:39 -0500

    Add padding check to hashes

--- test_hashes.py
+++ test_hashes.py
@@ -4,11 +4,13 @@ def assert_is_sorted(lst, key=lambda x: x):
 
 def test_hashes():
     from binascii import crc32
-    
+
     with open("ParamLabels.csv") as f:
         csv = [line.rstrip('\n').split(',') for line in f.readlines() if not line.isspace()]
-    
+
     assert_is_sorted(csv, key=lambda i: i[1])
     for line in csv:
+        hashString = line[0]
+        assert len(hashString) >= 12
         # Only the lower 32 bits are the hash (blame arthur), ensure the crc is legit
         assert crc32(line[1].encode('utf-8')) == (int(line[0], 16) & 0xFFFFFFFF)
