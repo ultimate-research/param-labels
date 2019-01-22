a04f4e8c8b75de1a6b5c56a4e6839235e09bee62
jam1garner <jam1.mcleod@hotmail.com>
2019-01-22 13:57:49 -0500

    Add non-pytest interface for printing all failed assertions

--- test_hashes.py
+++ test_hashes.py
@@ -13,4 +13,30 @@ def test_hashes():
         hashString = line[0]
         assert len(hashString) >= 12
         # Only the lower 32 bits are the hash (blame arthur), ensure the crc is legit
+        # length - uppermost 8  bits
+        # crc32  - lowermost 32 bits
         assert (len(line[1]) << 32 | crc32(line[1].encode('utf-8'))) == (int(line[0], 16))
+
+def main():
+    from binascii import crc32
+
+    with open("ParamLabels.csv") as f:
+        csv = [line.rstrip('\n').split(',') for line in f.readlines() if not line.isspace()]
+
+    try:
+        assert_is_sorted(csv, key=lambda i: i[1])
+    except AssertionError:
+        print("ParamLabels.csv is not sorted, run remove_duplicates.py to fix")
+    for i,line in enumerate(csv):
+        hashString = line[0]
+        if not len(hashString) >= 12:
+            print(f"'{hashString}', line {i+1} is not properly padded to 12 chars.")
+        # Only the lower 32 bits are the hash (blame arthur), ensure the crc is legit
+        if not (len(line[1]) << 32 | crc32(line[1].encode('utf-8'))) == (int(line[0], 16)):
+            print(f"'{hashString}', line {i+1} length or hash mismatch.")
+
+# Note: Intended use is with pytest, this is
+#       merely for printing out incorrect hashes
+if __name__ == '__main__':
+    main()
+
