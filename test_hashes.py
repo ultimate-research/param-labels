f4c03674bb22faa990409904064826039d25b20a
Shivam Dutt <shiv75035@gmail.com>
2021-02-15 13:49:28 -0600

    Rewrote python scripts to use sets instead of arrays to check for duplicate hashes, massively improving performance

--- test_hashes.py
+++ test_hashes.py
@@ -11,17 +11,18 @@ def test_hashes():
         csv = [line.rstrip('\n').split(',', 1) for line in f.readlines() if not line.isspace()]
 
     assert_is_sorted(csv, key=lambda i: i[1])
-    alreadyFoundHashes = []
+    alreadyFoundHashes = set()
     for line in csv:
         hashString = line[0]
+        hash = int(hashString, 16)
         assert len(hashString) >= 12
         # Only the lower 32 bits are the hash (blame arthur), ensure the crc is legit
         # length - uppermost 8  bits
         # crc32  - lowermost 32 bits
-        assert len(line[1]) == (int(line[0], 16) >> 32)
-        assert crc32(line[1].encode('utf-8')) == (int(line[0], 16) & 0xFFFFFFFF)
-        assert not int(hashString, 16) in alreadyFoundHashes
-        alreadyFoundHashes.append(int(hashString, 16))
+        assert len(line[1]) == (hash >> 32)
+        assert crc32(line[1].encode('utf-8')) == (hash & 0xFFFFFFFF)
+        assert not hash in alreadyFoundHashes
+        alreadyFoundHashes.add(hash)
 
 def main():
     from binascii import crc32
@@ -33,22 +34,22 @@ def main():
         assert_is_sorted(csv, key=lambda i: i[1])
     except AssertionError:
         print("ParamLabels.csv is not sorted, run remove_duplicates.py to fix")
-    alreadyFoundHashes = []
-    for i,line in enumerate(csv):
+    alreadyFoundHashes = set()
+    for i, line in enumerate(csv):
         hashString = line[0]
+        hash = int(hashString, 16)
         if not len(hashString) >= 12:
             print(f"'{hashString}', line {i+1} is not properly padded to 12 chars.")
-        if not len(line[1]) == (int(line[0], 16) >> 32):
+        if not len(line[1]) == (hash >> 32):
             print(f"'{hashString}', line {i+1} specified string length mismatch.")
         # Only the lower 32 bits are the hash (blame arthur), ensure the crc is legit
-        if not crc32(line[1].encode('utf-8')) == (int(line[0], 16) & 0xFFFFFFFF):
+        if not crc32(line[1].encode('utf-8')) == (hash & 0xFFFFFFFF):
             print(f"'{hashString}', line {i+1} crc32 mismatch.")
-        if int(hashString, 16) in alreadyFoundHashes:
+        if hash in alreadyFoundHashes:
             print(f"'{hashString}', line {i+1} hash duplicate.")
-        alreadyFoundHashes.append(int(hashString, 16))
+        alreadyFoundHashes.add(hash)
 
 # Note: Intended use is with pytest, this is
 #       merely for printing out incorrect hashes
 if __name__ == '__main__':
     main()
-
