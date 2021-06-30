d6705b3478dbf2942d9f0d06b2f030d816278a1f
Zarek Syed <zarek.syed@gmail.com>
2021-06-30 18:13:35 -0400

    SQB remove_duplicates.py
    Uses sets instead of lists for performance

--- sqb/remove_duplicates.py
+++ sqb/remove_duplicates.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
 with open("Labels.txt") as f:
-    csv = [line.rstrip('\n').split(',', 1) for line in f.readlines() if not line.isspace()]
-motionList = []
-for i in csv:
-    if not i in motionList:
-        motionList.append(i)
+    csv = [line.rstrip('\n') for line in f.readlines() if not line.isspace()]
 
-motionList.sort(key=lambda i: i)
+csvout = sorted(set(csv))
 
 with open("Labels.txt", 'w') as f:
-    f.write('\n'.join([','.join(line) for line in motionList]))
+    f.write('\n'.join(csvout))
